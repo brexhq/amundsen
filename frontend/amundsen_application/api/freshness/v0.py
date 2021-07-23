3afbc9a53e9800490bbb5e38f2c72d0ff2c57621
Junda Yang <jyang@brex.com>
2021-07-22 19:00:28 -0700

    add data freshness endpoint

--- frontend/amundsen_application/api/freshness/v0.py
+++ frontend/amundsen_application/api/freshness/v0.py
@@ -1,41 +1,72 @@
 # Copyright Contributors to the Amundsen project.
 # SPDX-License-Identifier: Apache-2.0
 
+import json
 import logging
 
 from http import HTTPStatus
 
-from flask import Response, jsonify, make_response
+from flask import Response, jsonify, make_response, request, current_app as app
 from flask.blueprints import Blueprint
+from marshmallow import ValidationError
+from werkzeug.utils import import_string
 
-from amundsen_application.models.preview_data import (
-    ColumnItem,
-    PreviewData,
-    PreviewDataSchema,
-)
+from amundsen_application.models.preview_data import PreviewDataSchema
+from amundsen_application.api.metadata.v0 import _get_table_metadata
 
 LOGGER = logging.getLogger(__name__)
+DATA_FRESHNESS_CLIENT_CLASS = None
+DATA_FRESHNESS_CLIENT_INSTANCE = None
 
 freshness_blueprint = Blueprint('freshness', __name__, url_prefix='/api/freshness/v0')
 
 
 @freshness_blueprint.route('/', methods=['POST'])
 def get_table_freshness() -> Response:
-    # fake data to test out functionality
-    columns = [
-        ColumnItem(column_name='latest updated_at', column_type=""),
-        ColumnItem(column_name='latest inserted_at', column_type=""),
-    ]
-    rows_to_dicts = [
-        {
-            'latest updated_at': '2021-07-14 13:52:51.807 +0000',
-            'latest inserted_at': '2021-07-13 10:01:03.302 +0000'
-        }
-    ]
-    preview_data = PreviewData(columns, rows_to_dicts)
+    global DATA_FRESHNESS_CLIENT_INSTANCE
+    global DATA_FRESHNESS_CLIENT_CLASS
 
-    data = PreviewDataSchema().dump(preview_data)
-    payload = jsonify({'freshnessData': data, 'msg': 'Success'})
-    import time
-    time.sleep(5)
-    return make_response(payload, HTTPStatus.OK)
+    try:
+        if DATA_FRESHNESS_CLIENT_INSTANCE is None:
+            if (app.config['DATA_FRESHNESS_CLIENT_ENABLED']
+                    and app.config['DATA_FRESHNESS_CLIENT'] is not None):
+                DATA_FRESHNESS_CLIENT_CLASS = import_string(app.config['DATA_FRESHNESS_CLIENT'])
+                DATA_FRESHNESS_CLIENT_INSTANCE = DATA_FRESHNESS_CLIENT_CLASS()
+            else:
+                payload = jsonify({'freshnessData': {}, 'msg': 'A client for the data freshness must be configured'})
+                return make_response(payload, HTTPStatus.NOT_IMPLEMENTED)
+
+        # get table metadata and pass to data_freshness_client
+        # data_freshness_client need to check if the table has any column
+        # that can be used to as freshness indicator
+        params = request.get_json()
+        if not all(param in params for param in ['database', 'cluster', 'schema', 'tableName']):
+            payload = jsonify({'freshnessData': {}, 'msg': 'Missing parameters in request payload'})
+            return make_response(payload, HTTPStatus.FORBIDDEN)
+
+        table_key = f'{params["database"]}://{params["cluster"]}.{params["schema"]}/{params["tableName"]}'
+        table_metadata = _get_table_metadata(table_key=table_key)
+
+        response = DATA_FRESHNESS_CLIENT_INSTANCE.get_freshness_data(params=table_metadata)
+        status_code = response.status_code
+
+        freshness_data = json.loads(response.data).get('freshness_data')
+        if status_code == HTTPStatus.OK:
+            # validate the returned data
+            try:
+                data = PreviewDataSchema().load(freshness_data)
+                payload = jsonify({'freshnessData': data, 'msg': 'Success'})
+            except ValidationError as err:
+                logging.error('Freshness data dump returned errors: ' + str(err.messages))
+                raise Exception('The data freshness client did not return a valid object')
+        else:
+            message = 'Encountered error: Freshness client request failed with code ' + str(status_code)
+            logging.error(message)
+            # only necessary to pass the error text
+            payload = jsonify({'freshnessData': {'error_text': freshness_data.get('error_text', '')}, 'msg': message})
+        return make_response(payload, status_code)
+    except Exception as e:
+        message = f'Encountered exception: {str(e)}'
+        logging.exception(message)
+        payload = jsonify({'freshnessData': {}, 'msg': message})
+        return make_response(payload, HTTPStatus.INTERNAL_SERVER_ERROR)
