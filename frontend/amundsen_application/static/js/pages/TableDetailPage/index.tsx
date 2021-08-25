70b713a06d11ea4699d6868dfc91b161da02d0ae
Junda Yang <youngyjd@gmail.com>
2021-08-25 10:22:42 -0700

    Delete default lineage button (#4)
    * disable lineage button
    
    * remove lineagebutton import
    
    Signed-off-by: Junda Yang <youngyjd@gmail.com>

--- frontend/amundsen_application/static/js/pages/TableDetailPage/index.tsx
+++ frontend/amundsen_application/static/js/pages/TableDetailPage/index.tsx
@@ -67,7 +67,6 @@ import {
 import DataPreviewButton from './DataPreviewButton';
 import DataFreshnessButton from './DataFreshnessButton';
 import ExploreButton from './ExploreButton';
-import LineageButton from './LineageButton';
 import FrequentUsers from './FrequentUsers';
 import LineageLink from './LineageLink';
 import LineageList from './LineageList';
@@ -461,7 +460,6 @@ export class TableDetail extends React.Component<
               <SourceLink tableSource={data.source} />
             </div>
             <div className="header-section header-buttons">
-              <LineageButton tableData={data} />
               <TableReportsDropdown resourceReports={data.resource_reports} />
               <DataFreshnessButton modalTitle={this.getDisplayName()} />
               <DataPreviewButton modalTitle={this.getDisplayName()} />
