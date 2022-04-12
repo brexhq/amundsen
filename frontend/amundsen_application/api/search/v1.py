9103b38ab5e033d8287e2524d1549b11b3769405
Junda Yang <jyang@brex.com>
2022-04-12 10:02:43 -0700

    update

--- frontend/amundsen_application/api/search/v1.py
+++ frontend/amundsen_application/api/search/v1.py
@@ -84,7 +84,7 @@ def _brex_custom_filter(field: str, field_filters: Dict[str, str]) -> Filter:
 
     if field == 'source':
         return Filter(name='database', values=values, operation=filter_operation)
-    elif field in ['database', 'version']:
+    elif field in ['db', 'version']:
         values = [f'{v}@*' for v in values]
         return Filter(name='schema', values=values, operation=filter_operation)
     elif field in ['schema', 'freshness']:
